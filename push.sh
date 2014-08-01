a48255b67499f1ec238682e8cb32bf1edbf0e933
Donald A. Lobo <lobo@civicrm.org>
2014-08-01 09:16:25 -0700

    Add invoice message stuff at year end

--- push.sh
+++ push.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-pushd ~/svn/school
-svn commit -m 'more SFS fixes' .
-svn up .
+pushd ~/git/sfschool
+git commit -a -m 'more SFS fixes' .
+git push master
 popd
 pushd /tmp
-rm -rf org.civicrm.school org.civicrm.school.tgz
-svn export http://svn.civicrm.org/sfschool/trunk/org.civicrm.school org.civicrm.school
+rm -rf sfschool
+git clone https://github.com/webaccess/sfschool.git
+cd sfschool
 tar czf org.civicrm.school.tgz org.civicrm.school
 scp org.civicrm.school.tgz sfschool@sfschool.org:/home/sfschool/www/drupal/sites/all/modules/extensions
 ssh sfschool@sfschool.org "cd /home/sfschool/www/drupal/sites/all/modules/extensions ; tar xzf org.civicrm.school.tgz"
 popd
-
